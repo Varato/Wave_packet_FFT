72c379e46eae2ed887ec70b6c902aa9b8da2b47e
Varato <imxinchen@outlook.com>
2016-11-07 23:48:03 +0800

    update

--- main/plot_transprob.py
+++ main/plot_transprob.py
@@ -3,5 +3,9 @@ import matplotlib.pyplot as plt
 data = np.loadtxt("transprob", delimiter=",")
 E = data[:,0]
 T = data[:,1]
-plt.plot(E,T,'bo')
+plt.plot(E,T,'r-.o')
+plt.xlabel("$E/V_0$")
+plt.ylabel("T")
+plt.title("Transmission Probability by FFT")
+plt.savefig("transprob1.png")
 plt.show()
